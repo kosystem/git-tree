a05965d1b21a72d4edf33c7dd5a109e9626659c4
Damien <damien@dam.io>
2021-04-17 08:39:38 +0200

    update to python 3

--- gitTree/gitTree.py
+++ gitTree/gitTree.py
@@ -22,10 +22,10 @@ def grouping(fileList):
 def displayItems(items, path, prefix, color):
     for index, item in enumerate(sorted(items.keys())):
         if index == len(items)-1:
-            print prefix + '└── ' + appendColor(path, item, color)
+            print(prefix + '└── ' + appendColor(path, item, color))
             nextPrefix = prefix + '    '
         else:
-            print prefix + '├── ' + appendColor(path, item, color)
+            print(prefix + '├── ' + appendColor(path, item, color))
             nextPrefix = prefix + '│   '
         if len(items[item]) > 0:
             nextpath = os.path.join(path, item)
@@ -59,14 +59,14 @@ def main():
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
     p.wait()
-    stdout_data = p.stdout.readlines()
+    stdout_data = [l.decode('utf-8') for l in p.stdout.readlines()]
     stderr_data = p.stderr.read()
     if len(stderr_data) > 0:
-        print stderr_data,
+        print(stderr_data, end=' ')
     else:
         color = True
         currentDir = os.path.split(os.getcwd())
-        print appendColor(currentDir[0], currentDir[1], color)
+        print(appendColor(currentDir[0], currentDir[1], color))
         group = grouping(stdout_data)
 
         displayItems(group, '.', '', color)
